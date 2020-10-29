b4b44a7fa9ec35d3147c7b0335e9eebe4feefca6
sanketh <jsanketh1799@gmail.com>
2020-10-29 18:37:09 +0530

    second

---
+++ src/Sidebar.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import './Sidebar.css'
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import { Avatar, IconButton } from '@material-ui/core';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import {SearchOutlined} from '@material-ui/icons';
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebar__header">
+            <Avatar src='https://avatars3.githubusercontent.com/u/45506004?s=400&u=cb792d5bd04eaeecc7de39f6c8376e56fc1750fc&v=4'/>
+            <div className="sidebar__headerRight">
+
+                <IconButton>
+                    <DonutLargeIcon />
+                </IconButton>
+                <IconButton>
+                    <ChatIcon />
+                </IconButton><IconButton>
+                    <MoreVertIcon />
+                </IconButton>
+            </div>
+            </div>
+
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="Search or create a new Group" type="text" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBk8QCrFKMSIh7rqyWBV8-QDdY9mzw3sew",
+    authDomain: "whatsapp-mern-e8eec.firebaseapp.com",
+    databaseURL: "https://whatsapp-mern-e8eec.firebaseio.com",
+    projectId: "whatsapp-mern-e8eec",
+    storageBucket: "whatsapp-mern-e8eec.appspot.com",
+    messagingSenderId: "949464309636",
+    appId: "1:949464309636:web:879fe8853de46e172c47d1",
+    measurementId: "G-HCWYLSP82L"
+  };
